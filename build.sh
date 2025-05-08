39289657af074c8b9713cf64c921ac949ba56aa7
cl0vrfi3ld <47996003+cl0vrfi3ld@users.noreply.github.com>
2025-05-07 23:21:36 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-mkarchiso -v -w workdir/ -o out/ .
+# mkarchiso -v -w workdir/ -o out/ .
+sudo podman run --rm -v ./out:/out -t -i --privileged ghcr.io/cl0vrfi3ld/container-archiso-builder:latest -r "https://github.com/cl0vrfi3ld/clos-image-builder.git" -b $1
